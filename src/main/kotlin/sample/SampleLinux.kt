8616f30fb508e79adc252b0f45317c7f80603408
kjanecki <janekjanecki14@gmail.com>
2020-06-03 22:56:47 +0200

    Write results to csv

--- src/main/kotlin/sample/SampleLinux.kt
+++ src/main/kotlin/sample/SampleLinux.kt
@@ -7,6 +7,8 @@ import sample.generator.RandomNeighbourhoodSelector
 import sample.generator.SimpleWorldNetworkGenerator
 import sample.model.NumericIndividual
 import sample.solver.CeaSolverImpl
+import java.io.FileWriter
+import java.io.IOException
 
 fun main() {
 
@@ -24,12 +26,13 @@ fun main() {
     var everBestNode : NumericIndividual = network.getNode(0) ?: error("Empty node list")
     var everBestValue : Double = Double.MIN_VALUE
 
+    val data = mutableListOf<String>()
     for (i in 1..100){
         runBlocking { ceaSolver.nextGeneration() }
         val bestNode = ceaSolver.getBestMatchedNode()
         val bestValue = ceaSolver.getCurrentValue()
         println("Generation $i: node $bestNode, value $bestValue")
-
+        data.add(bestValue.toString())
         if(bestValue.compareTo(everBestValue) > 0) {
             everBestIteration = i
             everBestNode = bestNode
@@ -38,4 +41,31 @@ fun main() {
     }
     println()
     println("Best value in generation $everBestIteration: node $everBestNode, value $everBestValue")
+
+    writeResultsToCsv(data)
+
+}
+
+private fun writeResultsToCsv(data: MutableList<String>) {
+    var fileWriter: FileWriter? = null
+
+    try {
+        fileWriter = FileWriter("test.csv")
+        fileWriter.append("generation_id,value\n");
+        for ((i, value) in data.withIndex()) {
+            fileWriter.append("${i+1},$value\n");
+        }
+
+    } catch (e: Exception) {
+        println("Writing CSV error!")
+        e.printStackTrace()
+    } finally {
+        try {
+            fileWriter!!.flush()
+            fileWriter.close()
+        } catch (e: IOException) {
+            println("Flushing/closing error!")
+            e.printStackTrace()
+        }
+    }
 }
